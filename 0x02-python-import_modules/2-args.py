da1b5b52416c691086a31772048e38a4ca3d327e
Mishael-2584 <worancha2584@student.babcock.edu.ng>
2022-06-08 18:10:12 -0700

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
             print("0 arguments.")
         else:
             if total == 2:
-                 print(f"{(total - 1):d} argument:")
+                print(f'{(total - 1):d} argument:')
             else:
-                print(f"{(total - 1):d} arguments:")
+                print(f'{(total - 1):d} arguments:')
             for i in range(1, total):
-                print(f"{i:d}: {argv[i]:s}")
+                print(f'{i:d}: {argv[i]:s}')
